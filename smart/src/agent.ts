a89313b9f2484670cffb807b24ddb8a4af6a6e23
Mar1cc <wuockhanhh5@gmail.com>
2026-09-03 17:05:09 +0700

    Xong CRUD KetQuaPhanTichCv,DonUngTuyen,DoanhNghiep,DanhMucNghe,DanhGia,CVUngVien

--- smart/src/agent.ts
+++
@@ -1,8 +0,0 @@
-import { HttpAgent } from "@ag-ui/client";
-
-/** Builds this starter's agent. See channel-host.mts for why this is shared. */
-export function createDefaultAgent(): HttpAgent {
-  return new HttpAgent({
-    url: process.env.AGENT_URL || "http://localhost:8000/",
-  });
-}
