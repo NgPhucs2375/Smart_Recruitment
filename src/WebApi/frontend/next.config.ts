cdfc13627759607ec84a4f2606378c36f9f20be0
Smart Recruitment Dev <dev@smart-recruitment.local>
2026-09-23 16:57:58 +0700

    Docker: sửa lỗi docker

--- src/WebApi/frontend/next.config.ts
+++ src/WebApi/frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   typescript: { ignoreBuildErrors: true },
   serverExternalPackages: ["@copilotkit/runtime"],
   allowedDevOrigins: ["localhost", "127.0.0.1"],
